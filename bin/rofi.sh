ee3b488c5a236fd0c52a2e2bc31ce109eed36329
Sebastian Schubert <sschubert932@gmail.com>
2016-06-19 16:33:06 +0200

    added some i3 stuff and scripts

---
+++ bin/rofi.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rofi -modi "run,drun" -show drun -width 1920 -fg "#B0E670" -bg "#14161F" -hlfg "#14161F" -hlbg "#B0E670" -bc "#14161F" -location 0 -lines 19 -bw 0 -font "Hack 13" -padding 400 -separator-style none -opacity 90
