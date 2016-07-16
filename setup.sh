c0c02ee2c03f406c1a1356afcf08e8687615640e
Sebastian Schubert <sschubert932@gmail.com>
2016-07-16 18:54:54 +0200

    updated setup script

--- setup.sh
+++ setup.sh
@@ -4,8 +4,10 @@
 # Setup script to simplify installation
 # v1
 ########################################
-
+echo "Installing pacaur"
 curl -s https://gist.githubusercontent.com/Tadly/0e65d30f279a34c33e9b/raw/pacaur_install.sh | bash
+
+echo "Installing dependencies for dotfiles"
 
 pacaur -Syu --noconfirm \
     git \
@@ -25,6 +27,70 @@ pacaur -Syu --noconfirm \
     deluge \
     dunst \
     mpv \
+    vim \
+    wpa_supplicant \
+    wpa_actiond \
+    xorg-xinit \
+    xorg-xrandr \
+    xorg-xbacklight \
+    xorg-xev \
+    xorg-xinput \
+    xorg-xset \
+    xorg-xsetroot \
+    xorg-xprop \
+    ttf-font-awesome \
+    rxvt-unicode-256xresources \
+    fira-code-git \
+    neofetch \
+    zsh \
+    prezto-git \
+    mps-youtube \
+    dhcpcd \
+    rofi \
+    ntfs-3g \
+    numlockx \
+    nvm-git \
+    python2 \
+    python \
+    pulseaudio \
+    pulseaudio-alsa \
+    pulseaudio-ctl \
+    amixer \
+    aspell \
+    aspell-de \
+    aspell-en \
+    arandr \
+    bashdb \
+    gtk-theme-arc-git \
+    i3lock \
+    mesa \
+    ncdu \
+    numix-icon-theme-git \
+    powerline-fonts-git \
+    ranger \
+    redshift \
+    rsync \
+    sbcl \
+    scrot \
+    skype \
+    telegram-desktop-bin \
+    termsyn-font \
+    envypn-font \
+    ttf-material-icons \
+    bdf-unifont \
+    xf86-input-keyboard \
+    xf86-input-mouse \
+    xf86-input-synaptics \
+    xf86-input-libinput \
+    xf86-video-intel \
+    xorg-setxkbmap \
+    compton
 
+echo "Cloning dots repository"
 
+git clone --recursive --separate-git-dir=$HOME/.dots.git https://github.com/sconos/dots.git /tmp/dots
+rsync -rvl --exclude ".git" /tmp/dots/ $HOME/
+rm -r /tmp/dots
+dots submodule update --init --recursive
 
+echo "Finished..."
